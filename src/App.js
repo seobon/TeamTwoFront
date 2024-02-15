f9e7664a8076c686e9eb566d94c9db025bda0721
kihet77 <mssk2490@gmail.com>
2024-02-15 15:47:23 +0900

    docs:  create

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Test from "./pages/testpage/Test.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+    </div>
+  );
+}
+
+export default App;
