5d427dc3c8555e3c4fe5e5d2bea6a3d6a7c14d7a
ldw0123 <ldw0123@gmail.com>
2024-02-15 21:17:37 +0900

    feat: 라우터

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Test from './pages/testpage/Test.jsx';
+import Calendar from 'react-calendar';
 
 function App() {
   return (
-    <div className="App">
-      <Test />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/test" element={<Test />} />
+        <Route path="/calender" element={<Calendar />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
