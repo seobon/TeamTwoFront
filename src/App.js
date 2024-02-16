7953182c4d5b5247c59da54ed5219e7e6e805b18
ldw0123 <ldw0123@gmail.com>
2024-02-16 22:18:59 +0900

    Feat: 캘린더, todo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Test from './pages/testpage/Test.jsx';
-import Calendar from 'react-calendar';
+import Todo from './pages/calendar/Todo.jsx';
+import Calendar1 from './pages/calendar/Calendar.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/test" element={<Test />} />
-        <Route path="/calender" element={<Calendar />} />
+        <Route path="/calendar" element={<Calendar1 />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/testpage/Test.jsx
+++ src/pages/testpage/Test.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Test = () => {
-  return <div className="container bg-gray-500">hi</div>;
+  return (
+    <>
+      <div className="container bg-gray-500">hi</div>
+      <div className="text-lg">Hello word</div>
+      <div className="text-2xl">Hello word</div>
+      <div className="text-9xl p-3">Hello world</div>
+      <div className="w-32 h-32 bg-red rounded-2xl"></div>
+      <br />
+      <div className="text-[33px] bg-[#6083c7] text-white mt-[10px]">Hello world</div>
+    </>
+  );
 };
 
 export default Test;
