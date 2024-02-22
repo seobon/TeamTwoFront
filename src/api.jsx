6990886ddf7f84dd72616b3f5f227c561cb0abfa
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-02-22 18:11:56 +0900

    Fix: conflict 오류 수정

--- src/api.jsx
+++ src/api.jsx
@@ -1,10 +1,5 @@
 //* 보드 리스트 불러올 리액트 쿼리 api
-import axios from 'axios';
 
-const URL = 'http://localhost:8080';
-
-// export async function getEveryDiary() {
 export const getEveryDiary = async () => {
-  return await fetch(`${URL}/diary/getEveryDiary`).then(res => res.json());
-  // return axios.get(`${URL}/diary/getEveryDiary`);
+  return await fetch(`${process.env.REACT_APP_HOST}/diary/getEveryDiary`).then(res => res.json());
 };
