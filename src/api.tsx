fab449b3716e555fef22d3093343a9f3ebe44b43
seobon <seobon1201@naver.com>
2024-02-22 21:51:44 +0900

    Feat: multer 코드 작성

--- src/api.tsx
+++
@@ -1,8 +0,0 @@
-//* 보드 리스트 불러올 리액트 쿼리 api
-
-const URL = ' ';
-
-export const get = async (path: string) => {
-  const response = await fetch(`${URL}/${path}`);
-  return await response.json();
-};
--- src/pages/write/Edit.jsx
+++
@@ -1,8 +0,0 @@
-// 글 수정 컴포넌트
-export default function Edit() {
-  return (
-    <>
-      <div></div>
-    </>
-  );
-}
