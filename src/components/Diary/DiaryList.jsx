7e5d81ee8d8fbfdb2be6036ae15633e9412b9af4
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-02-23 19:30:24 +0900

    Feat: react-query 무한 스크롤 구현 중 - 무한 스크롤 기초 작업 - 백엔드 코드 수정 필요

--- src/components/Diary/DiaryList.jsx
+++ src/components/Diary/DiaryList.jsx
@@ -1,19 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Diary from './Diary';
 import { Link } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import { getEveryDiary } from '../../api';
+import { useQuery, useInfiniteQuery } from '@tanstack/react-query';
+import { getEveryDiary, getEveryDiaryTest } from '../../api';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useInView } from 'react-intersection-observer';
 
 const BoardList = () => {
-  const { isLoading, data } = useQuery({ queryKey: ['diaries'], queryFn: getEveryDiary });
+  const { ref, inView } = useInView();
+
+  // const { isLoading, data } = useQuery({ queryKey: ['diaries'], queryFn: getEveryDiary });
+
+  // Todo : 무한 스크롤 구현을 위한 백엔드 코드 수정 필요
+  //* 무한 스크롤 구현을 위한 useInfiniteQuery 사용
+  //* inView가 화면에 보일 때마다 fetchNextPage() 호출
+  //* fetchNextPage() 호출 시 pageParam으로 다음 페이지 번호 전달
+  //* pageParm으로 전달된 페이지 번호를 기준으로 서버로부터 데이터를 받아오기
+
+  const { data, status, fetchNextPage, isFetchingNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ['diariesInfinite'],
+    queryFn: getEveryDiaryTest,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      console.log('allPages', allPages.length);
+      return lastPage.length === 0 ? undefined : allPages.length + 1;
+    },
+  });
+  const contents = data?.pages[0];
+  // console.log('contents', contents);
+  // const content = data?.pages.map(diaries => diaries.map(diary => diary));
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      // fetchNextPage();
+      console.log('inView', inView);
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
 
   return (
     <>
       <div>
-        {isLoading
+        {status === 'error' && <div>Error</div>}
+        {status === 'pending'
           ? 'Loading...'
-          : data?.map(diary => (
+          : contents?.map(diary => (
               <Link to={`/diary/${diary.diaryId}`}>
                 <Diary
                   key={diary.diaryId}
@@ -27,6 +58,9 @@ const BoardList = () => {
               </Link>
             ))}
       </div>
+      <button ref={ref} disabled={!hasNextPage || isFetchingNextPage}>
+        button
+      </button>
       <ReactQueryDevtools initialIsOpen />
     </>
   );
