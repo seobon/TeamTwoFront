0d216eb75c9f8b5b295f85d61c60076173e96d2b
한기선 <127398507+kihet77@users.noreply.github.com>
2024-02-24 14:53:00 +0900

    Revert "DESIGN: mypage 이미지 업로드"

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -4,7 +4,6 @@ import Header2 from '../../components/Header/Header2';
 import PopupNegative from '../../components/Popup/PopupNegative';
 import PopupLogout from '../../components/Popup/PopupLogout';
 import axios from 'axios';
-import defultImg from '../../assets/profileDefult.jpg';
 
 const Mypage = () => {
   const [closeLogoutPopup, showLogoutPopup] = useState(false);
@@ -14,9 +13,11 @@ const Mypage = () => {
   const userid = localStorage.getItem('userid');
   const id = localStorage.getItem('id');
 
+
   const [image, setImage] = useState();
   const [imagePath, setImagePath] = useState('');
   const ENV_URL = process.env.REACT_APP_DB_HOST;
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -39,12 +40,10 @@ const Mypage = () => {
   const userLogout = () => {
     showLogoutPopup(!closeLogoutPopup);
   };
-  const handleImgError = e => {
-    e.target.src = defultImg;
-  };
+
 
   // SB: 파일 전송 함수
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const formData = new FormData();
@@ -52,7 +51,7 @@ const Mypage = () => {
     formData.append('image', image);
 
     if (!image) {
-      alert('프로필로 사용할 이미지 파일을 업로드 해주세요.');
+      alert("프로필로 사용할 이미지 파일을 업로드 해주세요.")
     } else {
       try {
         const response = await axios.post(
@@ -63,19 +62,19 @@ const Mypage = () => {
             headers: {
               'Content-Type': 'multipart/form-data',
             },
-          },
+          }
         );
         setImagePath(response.data);
       } catch (error) {
-        alert('파일 전송에 실패했습니다. 잠시 후 다시 시도해주세요');
+        alert("파일 전송에 실패했습니다. 잠시 후 다시 시도해주세요")
       }
     }
   };
 
-  // SB: 파일 url을 useState로 저장하는 함수
-  const handleImageUpload = e => {
-    setImage(e.target.files[0]);
-  };
+    // SB: 파일 url을 useState로 저장하는 함수  
+    const handleImageUpload = (e) => {
+      setImage(e.target.files[0]);
+    };
 
 
   return (
@@ -83,39 +82,59 @@ const Mypage = () => {
       <Header2 title="마이페이지" />
       <div className="text-gray-600">
         <div className="flex justify-center">
-          <div className="w-32 h-32 bg-gray-200 bg-no-repeat bg-cover mb-6 rounded-full relative">
-            {/* SB: 파일 전송 폼 태그 */}
-            <form className="fileForm text-center" onSubmit={handleSubmit}>
+          <div className="w-32 h-32 bg-gray-200 mb-4 rounded-full">
+
+
+          {/* SB: 파일 전송 폼 태그 */}
+            <form className="fileForm" onSubmit={handleSubmit}>
               <label htmlFor="fileInput">
                 {!image && (
-                  <div className="fileExImage text-center">
-                    <img src="/static/exImage.png" alt="수정하기" className="rounded-full" onError={handleImgError} />
+                  <div className="fileExImage">
+                    <img
+                        src="/static/exImage.png"
+                        alt="img example"
+                        className="exImage"
+                        style={{ width: '65px', height: '50px' }}
+                      />
                   </div>
                 )}
-                <input id="fileInput" type="file" onChange={handleImageUpload} style={{ display: 'none' }} />
-                {image && (
-                  <img src={URL.createObjectURL(image)} alt="preview" className="rounded-full" style={{ width: '150px' }} />
-                )}
+                <input
+                    id="fileInput"
+                    type="file"
+                    onChange={handleImageUpload}
+                    style={{ display: 'none' }}
+                  />
+                  {image && (
+                    <img
+                      src={URL.createObjectURL(image)}
+                      alt="preview"
+                      className="exImage"
+                      style={{ width: '150px' }}
+                    />
+                  )}
               </label>
-              {image && (
-                <button type="submit" className="submitButton p-2">
-                  이미지 저장
-                </button>
-              )}
+              {/* SB: 전송 버튼 */}
+              <button type="submit" className="submitButton">
+                  이미지 업로드
+              </button>
             </form>
+
+
           </div>
         </div>
-        <p className="font-Body1 mb-6 text-center">{userInfo?.nickname} </p>
+        <p className="font-Body1 mb-6 text-center">{userInfo?.userid} id </p>
 
         <div className="rounded-lg mb-6 w-full bg-gray-200 p-3">
           <div className="relative">
             <p className="font-Caption mb-1.5">아이디</p>
             <p className="font-Body1 mb-6"> {userInfo?.userid} </p>
+            {/* <p className="font-Body1 mb-6"> id </p> */}
           </div>
 
           <div>
             <p className="font-Caption mb-1.5">이메일</p>
             <p className="font-Body1 mb-6"> {userInfo?.email} </p>
+            {/* <p className="font-Body1 mb-6"> email </p> */}
           </div>
 
           <button className="font-Caption mb-1.5" onClick={navigatePwChange}>
