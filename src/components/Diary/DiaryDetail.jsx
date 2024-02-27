e8405f16bc010d5beb11af9d575da36a4e199044
seobon <seobon1201@naver.com>
2024-02-27 14:07:40 +0900

    Feat : 상세 페이지 함수 완성 css 예정

--- src/components/Diary/DiaryDetail.jsx
+++ src/components/Diary/DiaryDetail.jsx
@@ -1,5 +1,5 @@
 // 게시판 상세 페이지
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Viewer } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 import { Link, useLocation, useParams, useMatch } from 'react-router-dom';
@@ -7,7 +7,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getEveryDiary } from '../../api';
 import Header1 from '../Header/Header1';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
 export default function BoardDetail() {
@@ -16,12 +16,21 @@ export default function BoardDetail() {
   const id = localStorage.getItem('id'); // 로컬 스토리지에서 id 값을 가져옴
 
   const [myDiaryInfo, setMyDiaryInfo] = useState({});
+  const [oneDiaryInfo, setOneDiaryInfo] = useState({});
 
+  const [whoDiary, setWhoDiary] = useState("");
+
+  const [title, setTitle] = useState("");
+  const [diaryTitle, setDiaryTitle] = useState("");
+  const [diaryContent, setDiaryContent] = useState("");
+  const [mood, setMood] = useState("");
+  const [weather, setWeather] = useState("");
+  const [createdAt, setCreatedAt] = useState("");
 
   // pthsname 정보와 상위 컴포넌트에서 정보를 state에 담아서 받아오기
   const state = useLocation();
   console.log('state', state);
-  const { diaryId, diaryTitle, diaryContent, image, mood, nickname } = state.state;
+  // const { diaryId, diaryTitle, diaryContent, image, mood, nickname } = state.state;
 
   // url에 있는 정보값 받아오기
   // const params = useParams();
@@ -33,20 +42,25 @@ export default function BoardDetail() {
     try {
       // Diary 작성자 정보를 가져오는 API 호출
       // SB : 추후에
-      // `${ENV_URL}/diary/checkUser?diaryId=12` 를
+      // `${ENV_URL}/diary/checkUser?diaryId=17` 를
       // `${ENV_URL}/diary/checkUser?diaryId=${diaryId}` 로
       // 수정하시면 되겠습니다.
       const response = await axios.get(
-        `${ENV_URL}/diary/checkUser?diaryId=12`
+        `${ENV_URL}/diary/checkUser?diaryId=17`
       );
   
       // SB: 콘솔 확인 부분입니다. 추후 삭제하시면 됩니다.
       console.log("checkUser data", response.data)
-
-      if (response.data == 1) {
+      console.log("id", id)
+      
+      if (response.data == id) {
         console.log("나의 다이어리 입니다.")
+        getMyDiary();
+        setWhoDiary("mine");
       } else {
         console.log("남의 다이어리 입니다.")
+        getOneDiary();
+        setWhoDiary("someone");
       }
   
     } catch (error) {
@@ -59,11 +73,11 @@ export default function BoardDetail() {
     try {
       // Diary 정보를 가져오는 API 호출
       // SB : 추후에
-      // `${ENV_URL}/diary/getMyDiary?diaryId=12` 를
+      // `${ENV_URL}/diary/getMyDiary?diaryId=17` 를
       // `${ENV_URL}/diary/getMyDiary?diaryId=${diaryId}` 로
       // 수정하시면 되겠습니다.
       const response = await axios.get(
-        `${ENV_URL}/diary/getMyDiary?diaryId=12`
+        `${ENV_URL}/diary/getMyDiary?diaryId=17`
       );
   
       // SB: 콘솔 확인 부분입니다. 추후 삭제하시면 됩니다.
@@ -77,18 +91,37 @@ export default function BoardDetail() {
         diaryTitle: response.data.diaryTitle, // 다이어리 제목
         diaryContent: response.data.diaryContent, // 다이어리 내용
         mood: response.data.mood, // 기분
+        weather: response.data.weather, // 날씨 정보 (SB : 현재는 제대로 된 정보가 들어오지 않습니다.)
+        public: response.data.public, // 공개 여부
         createdAt: response.data.createdAt, // 작성일
         updatedAt: response.data.updatedAt, // 수정일 (SB : 수정일 양식이 생성일과 다릅니다. 조만간 수정하겠습니다.)
-        weather: response.data.weather, // 날씨 정보 (SB : 현재는 제대로 된 정보가 들어오지 않습니다.)
         likey: response.data.likey, // likey 수
         love: response.data.love, // love 수
         haha: response.data.haha, // haha 수
         wow: response.data.wow, // wow 수
         sad: response.data.sad, // sad 수
         angry: response.data.angry, // angry 수
-        public: response.data.public, // 공개 여부
       });
-  
+
+      setTitle("일기")
+
+      let originDate = response.data.createdAt.split(' ')[0];
+      let month = response.data.createdAt.split(' ')[0].split('-')[1].replace('0', '');
+      let day = response.data.createdAt.split(' ')[0].split('-')[2];
+
+      var week = new Array('일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일');
+
+      var today = new Date(originDate).getDay();
+      var todayLabel = week[today];
+
+      let date = month + "월 " + day + "일 " + todayLabel;
+
+      setCreatedAt(date);
+
+      setDiaryTitle(response.data.diaryTitle);
+      setDiaryContent(response.data.diaryContent);
+      setMood(response.data.mood);
+      setWeather(response.data.weather);
     } catch (error) {
       console.error('Get My Diary Error:', error); // Diary 정보 가져오기 오류 출력
     }
@@ -103,14 +136,14 @@ export default function BoardDetail() {
       // `${ENV_URL}/diary/getOneDiary?diaryId=${diaryId}` 로
       // 수정하시면 되겠습니다.
       const response = await axios.get(
-        `${ENV_URL}/diary/getOneDiary?diaryId=12`
+        `${ENV_URL}/diary/getOneDiary?diaryId=16`
       );
   
       // SB: 콘솔 확인 부분입니다. 추후 삭제하시면 됩니다.
       console.log("getOneDiary data", response.data)
 
       // API 응답 데이터를 기반으로 다이어리 정보 설정
-      setMyDiaryInfo({
+      setOneDiaryInfo({
         nickname: response.data.nickname, // 유저 닉네임
         image: response.data.image, // 유저 이미지
         diaryId: response.data.diaryId, // 다이어리 아이디
@@ -124,20 +157,49 @@ export default function BoardDetail() {
         angry: response.data.angry, // angry 수
         public: response.data.public, // 공개 여부
       });
-  
+
+      setTitle(`${response.data.nickname}의 다이어리`)
+      setDiaryTitle(response.data.diaryTitle);
+      setDiaryContent(response.data.diaryContent);
     } catch (error) {
       console.error('Get One Diary Error:', error); // Diary 정보 가져오기 오류 출력
     }
   };
   
+  useEffect(() => {
+    checkUser();
+  }, []);
 
   return (
     <>
-      <Header1 title={`날짜 들어올 자리`} />
-      <div>{nickname}</div>
+      <Header1 title={title} />
+      {/* <div>{nickname}</div>
       <div>{diaryTitle}</div>
-      <div>{diaryContent}</div>
+      <div>{diaryContent}</div> */}
       {/* <Viewer initialValue={contents || ''} /> */}
+      <div>
+        {whoDiary === 'mine' ? (
+            <>
+            <div>{createdAt}</div>
+            <div style={{ backgroundColor: 'pink' }}>
+              <div>오늘의 기분</div>
+              <div>기분 이미지</div>
+              <div>{mood}</div>
+            </div>
+            <div style={{ backgroundColor: 'lightyellow' }}>
+              <div>오늘의 날씨</div>
+              <div>날씨 이미지</div>
+              <div>{weather}</div>
+            </div>
+            </>
+          ) : (
+            <></>
+          )}
+        <div style={{ backgroundColor: 'lightgreen' }}>
+          <div>{diaryTitle}</div>
+          <div>{diaryContent}</div>
+        </div>
+      </div>
     </>
   );
 }
