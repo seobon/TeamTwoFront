10a43b0cac2e570c66d9772a08cdce1c9e2263b5
kihet77 <mssk2490@gmail.com>
2024-02-27 11:52:46 +0900

    FEAT:회원가입 중복 확인

--- src/components/Header/Header2.jsx
+++ src/components/Header/Header2.jsx
@@ -10,13 +10,13 @@ const Header2 = ({ title }) => {
     <div className="px-4 py-3 mb-8">
       <p className='mb-4' onClick={onBeforePageHandler}>
         <svg
-          class="h-6 w-6 text-gray-600"
+          className="h-6 w-6 text-gray-600"
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round">
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round">
           {' '}
           <polyline points="15 18 9 12 15 6" />
         </svg>
