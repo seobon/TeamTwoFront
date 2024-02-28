dae171ec656b0862010baa762aac727adbd439eb
ldw0123 <ldw0123@gmail.com>
2024-02-28 13:57:01 +0900

    Revert "Merge pull request #75 from seobon/design/menu"
    This reverts commit 503e129a7c556a6a2f76002e3c9cfb72c4217eef, reversing
    git 롤백

--- src/Layout/Nav.jsx
+++ src/Layout/Nav.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Nav = ({ svg, title, onClick, stroke }) => {
+const Nav = ({ svg, title }) => {
   return (
-    <div className="text-center inline-block p-4" onClick={onClick}>
-      <div className='inline-block'>{svg}</div>
-      <div className={`mt-1 font-Caption w-full text-gray-500 `}>{title}</div>
+    <div className="text-center inline-block p-4">
+      <div className='inline-block '>{svg}</div>
+      <div className='mt-1 font-Caption w-full'>{title}</div>
     </div>
   );
 };
