3ecebdc3e46649d78ecf7386b2c132128b898de2
ldw0123 <ldw0123@gmail.com>
2024-02-29 03:25:55 +0900

    Design: 토글 버튼 / todo 스타일

--- src/pages/calendar/Todo.jsx
+++ src/pages/calendar/Todo.jsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { BsCheckCircle } from 'react-icons/bs';
+import { BsCircle } from 'react-icons/bs';
 import { useEffect, useState } from 'react';
 
 export default function Todo() {
@@ -156,20 +158,7 @@ export default function Todo() {
                       // setInputText(toDoList[index].text);
                       updateTodo(todo.todoId, 'notstart', todo.todoContent);
                     }}>
-                    <svg
-                      class="h-4 w-4 text-red-500"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      stroke-width="2"
-                      stroke="currentColor"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round">
-                      {' '}
-                      <path stroke="none" d="M0 0h24v24H0z" /> <circle cx="12" cy="12" r="9" />{' '}
-                      <path d="M9 12l2 2l4 -4" />
-                    </svg>
+                    <BsCheckCircle className="bg-[#ffd633] rounded-lg" />
                   </span>
                 ) : (
                   <span
@@ -178,19 +167,7 @@ export default function Todo() {
                       // setInputText(toDoList[index].text);
                       updateTodo(todo.todoId, 'done', todo.todoContent);
                     }}>
-                    <svg
-                      class="h-4 w-4 text-red-500"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      stroke-width="2"
-                      stroke="currentColor"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round">
-                      {' '}
-                      <path stroke="none" d="M0 0h24v24H0z" /> <circle cx="12" cy="12" r="9" />
-                    </svg>
+                    <BsCircle />
                   </span>
                 )}
               </button>
@@ -228,19 +205,7 @@ export default function Todo() {
         {editIndex === null && (
           <form onSubmit={handleAddSubmit} className="flex align-middle">
             <span className="mt-1">
-              <svg
-                class="h-4 w-4 text-red-500"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round">
-                {' '}
-                <path stroke="none" d="M0 0h24v24H0z" /> <circle cx="12" cy="12" r="9" />
-              </svg>
+              <BsCircle />
             </span>
             <input
               type="text"
