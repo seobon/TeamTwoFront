403aef3399d5f674d6b1e8083d74aa442615033b
ErrorMonkey <puts.hello.world.return0@gmail.com>
2024-02-28 22:28:44 +0900

    Feat: Search List 구현

--- src/api.jsx
+++ src/api.jsx
@@ -1,8 +1,8 @@
 //* 보드 리스트 불러올 리액트 쿼리 api
 
-export const getEveryDiary = async () => {
-  return await fetch(`${process.env.REACT_APP_HOST}/diary/getEveryDiary`).then(res => res.json());
-};
+// export const getEveryDiary = async () => {
+//   return await fetch(`${process.env.REACT_APP_HOST}/diary/getEveryDiary`).then(res => res.json());
+// };
 
 export const getEveryDiaryInfinite = async ({ pageParam }) => {
   return await fetch(`${process.env.REACT_APP_HOST}/diary/getEveryDiary`).then(res => res.json());
